c5b2b922f2fdcc00e34d48d1832f765317649a31
mouad2023se <elharrakmouad@gmail.com>
2025-01-25 20:58:51 +0100

    app update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import Welcome from "./components/Welcome";
 import ChatRoomRoutes from "./ChatRoomRoutes";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   if (loading) {
     return (
